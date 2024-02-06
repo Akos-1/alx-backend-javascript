b85621a93c8bf0fd73648807b17f57bd1c1a8f0b
Akos-1 <veronicagyasi@gmail.com>
2024-02-06 14:07:16 -0800

    update

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,7 +1,7 @@
 export default function createReportObject(employeesList) {
   return {
     allEmployees: {
-      ...employeesLisst,
+      ...employeesList,
     },
     getNumberOfDepartments(employeesList) {
       return Object.keys(employeesList).length;
