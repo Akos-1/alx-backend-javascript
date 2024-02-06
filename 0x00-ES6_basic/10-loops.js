0d9e6c3585f7a74415ff0abdf7ab6c1fe1d6e539
Akos-1 <veronicagyasi@gmail.com>
2024-02-06 13:45:10 -0800

    updated

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -2,7 +2,7 @@ export default function appendToEachArrayValue(array, appendString) {
   const newArray = array;
   for (const value of array) {
     const idx = array.indexOf(value);
-    newArray.[idx] =appendString + value;
+    newArray[idx] = appendString + value;
   }
 
   return newArray;
---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,6 @@
+export default function createReportObject(employeesList) {
+  const reportObject = {
+    employees: employeesList
+  };
+  return reportObject;
+}
