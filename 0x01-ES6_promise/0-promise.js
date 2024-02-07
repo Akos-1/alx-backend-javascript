9c02f6f1bf5395d3fa04428fd1973c4d8a645191
Akos-1 <veronicagyasi@gmail.com>
2024-02-07 14:00:14 -0800

    errors fixed

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,13 +1,8 @@
-function getResponseFromAPI() {
+function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
-    // Simulating an API call with setTimeout
-    setTimeout(() => {
-      const success = true;
-      if (success) {
-        resolve("Success! Data received from API.");
-      } else {
-        reject("Error! Failed to fetch data from API.");
-      }
-    }, 2000);
+    if (success) resolve({ status: 200, body: 'Success' });
+    reject(Error('Error! Failed to fetch data from API.'));
   });
 }
+
+export default getFullResponseFromAPI;
