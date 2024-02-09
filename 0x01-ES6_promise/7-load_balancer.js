3d3a56e72bda58388e1c81a9accb53cb66d205be
Akos-1 <veronicagyasi@gmail.com>
2024-02-09 13:49:14 -0800

    updated

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -1,3 +1,7 @@
-export default function loadBalancer(chunaDownload, USDownload) {
+// utils.js
+
+// Function to balance between two downloads
+export default function loadBalancer(chinaDownload, USDownload) {
+  // Return a Promise that resolves to the value of the first resolved Promise
   return Promise.race([chinaDownload, USDownload]);
 }
