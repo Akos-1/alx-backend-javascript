be3d89296d057eb36b492f6ba406943e9c7f2f3e
Akos-1 <veronicagyasi@gmail.com>
2024-02-10 07:21:48 -0800

    update

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,13 +1,12 @@
 export default class Building {
   constructor(sqft) {
-    this._sqft = typeof sqft === 'number' ? sqft : 0;
+    if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
+      throw new Erro('Class extending Building must override evacuationWarningMessage');
+    }
+    this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
-  }
-
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
