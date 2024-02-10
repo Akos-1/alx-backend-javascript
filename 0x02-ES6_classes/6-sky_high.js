1c14df41da158702a93ee21b456c76956b4b2e66
Akos-1 <veronicagyasi@gmail.com>
2024-02-10 07:30:51 -0800

    update

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -3,15 +3,18 @@ import Building from './5-building';
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
-    this._floors = typeof floors === 'number' ? floors : 0;
-  }
-
-  get sqft() {
-    return this._sqft;
+    this._floors = floors;
   }
 
   get floors() {
     return this._floors;
+  }
+
+  set floors(newFloors) {
+    if (typeof newFloors !== 'number') {
+      throw TypeError('Floors must be a number');
+    }
+    this._floors = newFloors;
   }
 
   evacuationWarningMessage() {
