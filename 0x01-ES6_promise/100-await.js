e92b25ddeb6048259363b76cbb3c6a9970fef096
Akos-1 <veronicagyasi@gmail.com>
2024-02-10 04:26:04 -0800

    updated

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,14 +1,22 @@
-import { uploadPhoto, createUser } from './utils.js';
+import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-  let photo;
-  let user;
   try {
-    photo = await uploadPhoto;
-    user = await createUser;
+    // Call the uploadPhoto and createUser functions asynchronously
+    const photoResponse = await uploadPhoto();
+    const userResponse = await createUser();
+
+    // Return an object with the responses
+    return {
+      photo: photoResponse,
+      user: userResponse,
+    };
   } catch (error) {
-    photo: null,
-    user: null
+    // If an error occurs, return an object with null values
+    console.error(error); // Log the error if needed
+    return {
+      photo: null,
+      user: null,
+    };
   }
-  return { photo, user};
 }
