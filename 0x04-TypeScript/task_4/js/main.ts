457d5faf13d30d03603b0954669bdd9b62516cb4
Akos-1 <veronicagyasi@gmail.com>
2024-02-18 06:05:50 -0800

    updated

---
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -0,0 +1,23 @@
+import { Subjects } from './subjects/Subject';
+
+export const cpp = new Subjects.Cpp();
+export const java = new Subjects.Java();
+export const react = new Subjects.React();
+
+export const cTeacher: Subjects.Teacher = { firstName: "John", lastName: "Doe", experienceTeachingC: 10 };
+
+console.log("Cpp:");
+cpp.setTeacher(cTeacher);
+console.log(cpp.getRequirements());
+console.log(cpp.getAvailableTeacher());
+
+console.log("\nJava:");
+java.setTeacher(cTeacher);
+console.log(java.getRequirements());
+console.log(java.getAvailableTeacher());
+
+console.log("\nReact:");
+react.setTeacher(cTeacher);
+console.log(react.getRequirements());
+console.log(react.getAvailableTeacher());
+
