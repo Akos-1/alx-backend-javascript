fa65dfca9d439fca78aeb293854f3058e1f69c4d
Akos-1 <veronicagyasi@gmail.com>
2024-02-17 22:23:54 -0800

    in progress

---
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -0,0 +1,13 @@
+// main.ts
+import { RowID, RowElement } from './interface';
+
+// Example usage of RowID and RowElement
+const rowID: RowID = 1;
+const row: RowElement = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 30
+};
+
+console.log("Row ID:", rowID);
+console.log("Row Element:", row);
