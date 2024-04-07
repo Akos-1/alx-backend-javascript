04ed934999cd0ac31417717903eace5decbf673a
Akos-1 <veronicagyasi@gmail.com>
2024-04-07 11:33:54 -0700

    updated

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,17 +1,16 @@
 // src/1-stdin.js
 
-// Function to handle input from the user
-function getInput(prompt) {
-  return new Promise(resolve => {
-    const readline = require('readline');
-    const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout
-    });
+const readline = require('readline');
 
-    rl.question(prompt, userInput => {
-      resolve(userInput);
-      rl.close();
-    });
-  });
-}
+console.log('Welcome to Holberton School, what is your name?');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('', answer => {
+  console.log(`Your name is: ${answer}`);
+  console.log('This important software is now closing');
+  rl.close();
+});
